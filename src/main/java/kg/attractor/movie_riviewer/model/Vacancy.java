945f9fe6dc250f8b4f4a17efed73709c4ea802bb
Kanat <mambetovkanat7@gmail.com>
2026-03-11 21:50:43 +0600

    add data models for JobSearch project

---
+++ src/main/java/kg/attractor/movie_riviewer/model/Vacancy.java
@@ -0,0 +1,21 @@
+package kg.attractor.movie_riviewer.model;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class Vacancy {
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer categoryId;
+    private Double salary;
+    private Integer expFrom;
+    private Integer expTo;
+    private Boolean isActive;
+    private Integer authorId;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateTime;
+}
